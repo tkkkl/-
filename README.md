0ac281b05aa3620737442c8e2708dc1604768cc0
tkkkl <tangkl.kailintang@gmail.com>
2023-10-10 01:57:07 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LaTex_tempate_for_UR
+ A LaTex template for the concluding report of undergraduate research at PKU.
---
+++ code/code2.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pylab as plt
+def func1():
+    """这是一个函数"""
+    pass
+plt.plot(np.arange(5),label = 'curve A')
+plt.show()
